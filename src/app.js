1520d39c2731a0278bfb02b64279485d65b69fa5
Andrew Elkouri <andrewelkouri@gmail.com>
2020-06-13 19:22:47 -0400

    clean up

--- src/app.js
+++ src/app.js
@@ -9,18 +9,3 @@ app.use(express.static('public'));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
-
-/*
-navigator.mediaDevices.getUserMedia(
-    { video: true, audio: true },
-    stream => {
-        const localVideo = document.getElementById("local-video");
-        if (localVideo) {
-            localVideo.srcObject = stream;
-        }
-    },
-    error => {
-        console.warn(error.message);
-    }
-);
-*/
